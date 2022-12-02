aa08289059ff6af4269565afcdca2c54f63af878
Panos Stavrianos <pstavrianosmail@gmail.com>
2022-12-02 22:01:40 +0200

    init

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="DirectusPyWrapper-panos-stavrianos",  # Replace with your own username
+    version="0.0.1",
+    author="Panos Stavrianos",
+    description="A python wrapper for Directus API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/panos-stavrianos/DirectusPyWrapper",
+    packages=setuptools.find_packages(),
+    classifiers=["directus", "wrapper", "api"],
+    python_requires='>=3.6',
+)
